187fa755caec4fef0ab01cce40c9e480f9ef7193
MKarimi <mkarimi21@hotmail.com>
2021-11-24 22:45:10 +0330

    start main page

---
+++ MCI_Project_Part01/None.py
@@ -0,0 +1,67 @@
+
+
+# Cell
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# Cell
+# from sklearn.preprocessing import MinMaxScaler
+
+
+
+
+# Cell
+# import sweetviz as sv
+
+# Cell
+# from pycaret.classification import *
+
+# Cell
+
+
+
+# Cell
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# Cell
+# from sklearn.preprocessing import MinMaxScaler
+
+
+
+
+# Cell
+# import sweetviz as sv
+
+# Cell
+# from pycaret.classification import *
+
+# Cell
+
+
+
+# Cell
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# Cell
+# from sklearn.preprocessing import MinMaxScaler
+
+
+
+
+# Cell
+# import sweetviz as sv
+
+# Cell
+# from pycaret.classification import *
+
+# Cell
+import pandas as pd
+
+
+# Cell
+import pandas as pd
